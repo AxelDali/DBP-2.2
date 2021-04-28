61c17d5cb2a73625af80e000ecb34cb53ede5ef2
Axel Dalí <a329881@uach.mx>
2021-04-27 23:44:03 -0600

    28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.

---
+++ 28.js
@@ -0,0 +1,6 @@
+// 28.- Simular el lanzamiento de una moneda al aire e imprimir cara o cruz.
+let moneda = Math.floor(Math.random() * 2);
+if(moneda == 1)
+    console.log("cara");
+else
+    console.log("cruz");
