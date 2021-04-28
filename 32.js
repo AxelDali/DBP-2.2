430dd1b4565665c9222a8576c1caef71b1d65f68
Axel Dalí <a329881@uach.mx>
2021-04-27 23:47:04 -0600

    32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,8 @@
+/* 32.- Crear un arreglo de 20 elementos con nombres de personas. Mostrar los
+    elementos de la lista debiendo ir cada uno en una fila distinta. */
+
+const array = ["Dalí", "Martín", "Marina", "Daniel", "Melissa", "Luis", "Juan",
+            "Maritrini", "Humberto", "Norma", "Raúl", "Ximena", "Antonio",
+            "Enrique", "Axel", "Rubén", "Sergio", "José", "Ricardo", "Jesus"];
+
+array.forEach(element => console.log(element));
