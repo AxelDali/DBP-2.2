dbafdc91ad1b78257da28392de1a359a45ff88e4
Axel Dalí <a329881@uach.mx>
2021-04-27 23:40:19 -0600

    16.- Imprimir diez veces la serie de números del 1 al 10.

---
+++ 16.js
@@ -0,0 +1,7 @@
+// 16.- Imprimir diez veces la serie de números del 1 al 10.
+for(let i = 1 ; i <= 10 ; i++){
+    console.log();
+    for(let j = 1 ; j <= 10 ; j++){
+        console.log(j);
+    }
+}
